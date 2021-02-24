a3a17d0a974822c2852693c4715fdba19d9680bf
miloszKubiak <milypan69@gmail.com>
2021-02-24 19:02:11 +0100

    End of the project

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const changeBtn = document.querySelector('.change');
 const spanOne = document.querySelector('.one');
 const spanTwo = document.querySelector('.two');
 let fahrenheit;
-let celcius;
+let celsius;
 
 const swap = () => {
     if (spanOne.innerText === '°C') {
@@ -27,8 +27,8 @@ const celToFahr = () => {
 };
 
 const fahrToCel = () => {
-    celcius = (converter.value * - 32) / 1.8;
-    result.innerText = `${converter.value}°F is ${celcius.toFixed(1)}°C`;
+    celsius = (converter.value * - 32) / 1.8;
+    result.innerText = `${converter.value}°F is ${celsius.toFixed(1)}°C`;
     converter.value = '';    
 };
 
